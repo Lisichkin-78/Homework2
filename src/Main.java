c57eff1ce04898e602b794a177e161e28a757b3f
Lisichkin-78 <dmitryalexxandrov@gmail.com>
2025-03-11 22:00:29 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,82 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        var dog = 8.0;
+        System.out.println (dog);
+        var cat = 3.6;
+        System.out.println (cat);
+        var paper = 763789;
+        System.out.println (paper);
+        dog = dog * 4;
+        System.out.println (dog);
+        cat = cat * 4;
+        System.out.println (cat);
+        paper = paper * 4;
+        System.out.println (paper);
+        dog = dog - 3.5;
+        System.out.println (dog);
+        cat = cat -1.6;
+        System.out.println(cat);
+        paper = paper - 7639;
+        System.out.println(paper);
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend+2;
+        System.out.println(friend);
+        friend=friend/7;
+        System.out.println(friend);
+        var frog =3.5;
+        System.out.println(frog);
+        frog=frog*10;
+        System.out.println(frog);
+        frog = frog/3.5;
+        System.out.println(frog);
+        frog = frog + 4;
+        System.out.println(frog);
+        var x=78.2;//вес 1 боксера
+        System.out.println(x);
+        var y=82.7;//вес 2 боксера
+        System.out.println(y);
+        System.out.println(x+y);
+        System.out.println(y-x);
+        System.out.println(y%x);
+        var wH=640;//общие часы
+        var w1=8;// часов на 1 человека
+        var wU=wH/w1;//количество людей
+        System.out.println(" Всего работников в компании " + wU + " человек.");
+        wU=wU+94;
+        System.out.println(wU);//самопроверка
+        wH=wU*w1;
+        System.out.println(" Если в компании работает " + wU + " человек, то всего " + wH + " часов работы может быть поделено между сотрудниками. ");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     }
 }
+
